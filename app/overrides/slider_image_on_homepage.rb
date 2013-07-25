eb3bc7690adcc93d073305a8fe902964fd728b35
Josh Teng <joshteng@webermize.com>
2013-07-25 18:40:24 +0800

    slider to bottom of wrapper

--- app/overrides/slider_image_on_homepage.rb
+++ app/overrides/slider_image_on_homepage.rb
@@ -1,4 +1,4 @@
 Deface::Override.new(:virtual_path  => "spree/layouts/spree_application",
-                     :insert_top => "div#wrapper",
+                     :insert_bottom => "div#wrapper",
                      :partial       => "../views/spree/store/home/slider",
                      :name          => "add_slider_to_homepage")
